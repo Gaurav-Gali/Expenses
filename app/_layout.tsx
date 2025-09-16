797c47daa9963c1c8e360966b605eba540caedb1
Gaurav Gali <128993184+Gaurav-Gali@users.noreply.github.com>
2025-09-16 15:06:21 +0530

    initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Stack} from "expo-router";
+import "@/global.css";
+
+function _layout() {
+    return (
+        <Stack></Stack>
+    );
+}
+
+export default _layout;
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+function Index() {
+    return (
+        <View>
+            <Text className={"text-blue-600"}>Hello World</Text>
+        </View>
+    );
+}
+
+export default Index;
